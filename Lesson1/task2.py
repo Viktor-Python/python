9d51d818dba46be01359b5677ca4218a758dabb1
Viktor-Python <laznikov1979@mail.ru>
2021-02-27 20:00:16 +0300

    ЗАДАНИЕ 2

--- Lesson1/task2.py
+++ Lesson1/task2.py
@@ -5,7 +5,9 @@
 Спросить у пользователя число и вывести в ответ сумму цифр этого числа.
 Программа должна спрашивать числа у пользователя до тех пор, пока он не введет "0".
 """
-numbers = None
-while numbers != 0:
-    numbers = sum(map(int, list(input("Введите число: "))))
-    print(numbers)
+numbers = int(input("Введите число: "))
+sum_of_dig = 0
+while numbers > 0:
+    sum_of_dig += numbers % 10
+    numbers //= 10
+print(sum_of_dig)
